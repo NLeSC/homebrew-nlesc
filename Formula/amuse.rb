b2c7a464fa285d59a880254736fb4cb26ab44305
Merijn Verstraaten <merijn@inconsistent.nl>
2019-03-14 14:20:23 +0100

    Initial commit of amuse formula.

--- Formula/amuse.rb
+++ Formula/amuse.rb
@@ -6,6 +6,9 @@ class Amuse < Formula
   url "https://github.com/amusecode/amuse.git", :tag => "v11.3.5"
   head  "https://github.com/amusecode/amuse.git"
 
+  hdf5 = "nlesc/nlesc/hdf5"
+  netcdf = "nlesc/nlesc/netcdf"
+
   depends_on "cmake" => :build
   depends_on "gcc" # for gfortran
   depends_on "python3" => :optional
@@ -13,10 +16,62 @@ class Amuse < Formula
   depends_on "fftw"
   depends_on "gsl"
   depends_on "jupyter"
-  depends_on "nlesc/nlesc/hdf5" => ["with-open-mpi"]
-  depends_on "nlesc/nlesc/netcdf"
+  depends_on hdf5 => ["with-open-mpi"]
+  depends_on netcdf
 
-  patch :DATA
+  patch <<-END_PATCH
+--- a/amuse-env   2019-03-12 13:49:49.000000000 +0100
++++ b/amuse-env   2019-03-12 13:49:37.000000000 +0100
+@@ -0,0 +1,48 @@
++. LIBEXEC_PREFIX/bin/activate
++
++prepend_path () {
++    eval $1=\\"$2${!1:+":"}${!1}\\"
++}
++
++extend_path () {
++  if ! [ -z "${!1+_}" ]; then
++      eval _OLD_VIRTUAL_$1="${!1}"
++  fi
++
++  prepend_path "$1" "$2"
++}
++
++eval 'deactivate () {
++    '"$(declare -f deactivate | tail -n+2)"'
++
++    reset_path () {
++        local OLDNAME="_OLD_VIRTUAL_$1"
++        if [ -z "${!OLDNAME+_}" ]; then
++            unset $1
++        else
++            eval $1="${!OLDNAME}"
++            unset $OLDNAME
++        fi
++    }
++
++    reset_path PKG_CONFIG_PATH
++    reset_path C_INCLUDE_PATH
++    reset_path CPLUS_INCLUDE_PATH
++    reset_path LIBRARY_PATH
++
++    unset -f reset_path
++}'
++
++prepend_path PATH "#{Formula[netcdf].bin}:#{Formula[hdf5].bin}"
++
++extend_path PKG_CONFIG_PATH "#{Formula[netcdf].lib}/pkgconfig"
++extend_path C_INCLUDE_PATH "#{Formula[netcdf].include}:#{Formula[hdf5].include}"
++extend_path CPLUS_INCLUDE_PATH "#{Formula[netcdf].include}:#{Formula[hdf5].include}"
++extend_path LIBRARY_PATH "#{Formula[netcdf].lib}:#{Formula[hdf5].lib}"
++
++unset -f prepend_path extend_path
++
++export PATH PKG_CONFIG_PATH C_INCLUDE_PATH CPLUS_INCLUDE_PATH LIBRARY_PATH
++if [ -n "${BASH-}" ] || [ -n "${ZSH_VERSION-}" ] ; then
++    hash -r 2>/dev/null
++fi
+END_PATCH
 
   def install
     venv = virtualenv_create(libexec)
@@ -26,6 +81,8 @@ class Amuse < Formula
 
     inreplace "#{libexec}/bin/activate", "`basename \\\"$VIRTUAL_ENV\\\"`",
       "AMUSE"
+
+    ohai "#{Formula["nlesc/nlesc/netcdf"].lib}"
 
     inreplace "amuse-env", "LIBEXEC_PREFIX", libexec
     bin.mkpath
@@ -55,65 +112,3 @@ EOS
     system "false"
   end
 end
-
-__END__
---- a/amuse-env   2019-03-12 13:49:49.000000000 +0100
-+++ b/amuse-env   2019-03-12 13:49:37.000000000 +0100
-@@ -0,0 +1,57 @@
-+. LIBEXEC_PREFIX/bin/activate
-+
-+prepend_path () {
-+    eval $1=\"$2${!1:+":"}${!1}\"
-+}
-+
-+extend_path () {
-+  if [ -z "${!1+_}" ]; then
-+      eval _OLD_VIRTUAL_$1="${!1}"
-+  fi
-+
-+  prepend_path "$1" "$2"
-+}
-+
-+reset_path () {
-+    local OLDNAME="_OLD_VIRTUAL_$1"
-+    if [ -z "${!OLDNAME+_}" ]; then
-+        eval $1="${!OLDNAME}"
-+        unset $OLDNAME
-+    else
-+        unset $1
-+    fi
-+}
-+
-+rename_function () {
-+    local ORIGINAL=$(declare -f $1)
-+    local NEW_FUNC="$2${ORIGINAL#$1}"
-+    eval "$NEW_FUNC"
-+}
-+
-+rename_function deactivate _old_virtual_
-+
-+deactivate () {
-+    _old_virtual_deactivate
-+
-+    reset_path PKG_CONFIG_PATH
-+    reset_path C_INCLUDE_PATH
-+    reset_path CPLUS_INCLUDE_PATH
-+
-+    unset -f _old_virtual_deactivate reset_path
-+}
-+
-+prepend_path PATH "/usr/local/opt/netcdf/bin"
-+prepend_path PATH "/usr/local/opt/hdf5/bin"
-+
-+extend_path PKG_CONFIG_PATH "/usr/local/opt/netcdf/lib/pkgconfig"
-+extend_path C_INCLUDE_PATH "/usr/local/opt/netcdf/include"
-+extend_path C_INCLUDE_PATH "/usr/local/opt/hdf5/include"
-+extend_path CPLUS_INCLUDE_PATH "/usr/local/opt/netcdf/include"
-+extend_path CPLUS_INCLUDE_PATH "/usr/local/opt/hdf5/include"
-+
-+unset -f prepend_path extend_path rename_function
-+
-+export PATH PKG_CONFIG_PATH C_INCLUDE_PATH CPLUS_INCLUDE_PATH
-+if [ -n "${BASH-}" ] || [ -n "${ZSH_VERSION-}" ] ; then
-+    hash -r 2>/dev/null
-+fi
